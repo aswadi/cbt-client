4bbe7726ae206e7dc3b69cc0d4f6328a17a88dd8
aswadi <genta.blog@gmail.com>
2021-03-24 14:06:06 +0700

    change footer label

--- app/Views/layout/template.php
+++ app/Views/layout/template.php
@@ -83,7 +83,7 @@
 
 <footer class="footer mt-auto py-3 bg-light">
   <div class="container">
-    <span class="text-muted">Place sticky footer content here.</span>
+    <span class="text-muted">Develop By PUSTIPD @ 2021.</span>
   </div>
 </footer>
 
