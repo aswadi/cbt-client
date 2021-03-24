5c6c3c30aa77a3cf8715d26b92d6cf18d47e11a8
aswadi <genta.blog@gmail.com>
2021-03-24 14:21:50 +0700

    add file ujian in viewa

---
+++ app/Views/exam/ujian.php
@@ -0,0 +1,23 @@
+<?= $this->extend('layout/template') ?>
+
+<?= $this->section('content') ?>
+    <div class="header">
+        <h3>Ujian berbasis komputer, UIN Raden Fatah Palembang</h3>
+    </div>
+
+    <div class="wrapper">
+        <div class="question">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, voluptatibus. Placeat consectetur, obcaecati a laudantium voluptatem rem, accusantium, consequatur voluptates quod culpa rerum fugit esse doloribus maxime corrupti dicta enim!
+
+        </div>
+        <div class="answer">
+            <ul>
+                <li>A. dsdw</li>
+                <li>B. dsdw</li>
+                <li>C. dsdw</li>
+            </ul>
+        
+        </div>
+    </div>
+
+<?= $this->endSection() ?>
