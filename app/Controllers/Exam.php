07fb7e92bceb4851d887f0c8bc4420ffba4f3fda
aswadi <genta.blog@gmail.com>
2021-03-24 16:11:17 +0700

    set new tem

--- app/Controllers/Exam.php
+++ app/Controllers/Exam.php
@@ -9,4 +9,12 @@ class Exam extends BaseController
         $data['title'] = 'CBT Raden Fatah';
 		return view('exam/index', $data);
 	}
+
+    public function test()
+	{
+        $data['title'] = 'CBT Raden Fatah';
+		return view('exam/test', $data);
+	}
+
+    
 }
--- app/Views/exam/test.php
+++ app/Views/exam/test.php
@@ -6,17 +6,35 @@
     </div>
 
     <div class="wrapper">
-        <div class="question">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, voluptatibus. Placeat consectetur, obcaecati a laudantium voluptatem rem, accusantium, consequatur voluptates quod culpa rerum fugit esse doloribus maxime corrupti dicta enim!
+        <div class="row">
+            <div class="col-md-9" id="wrapquestion">
+                <div class="number_question">
+                    <span>SOAL NO. 13</span>
+                    <hr>
+                </div>
+                <div class="question">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, voluptatibus. Placeat consectetur, obcaecati a laudantium voluptatem rem, accusantium, consequatur voluptates quod culpa rerum fugit esse doloribus maxime corrupti dicta enim!
+                    <hr>
+                </div>
+                <div class="answer">
+                    <ul>
+                        <li>A. dsdw</li>
+                        <li>B. dsdw</li>
+                        <li>C. dsdw</li>
+                    </ul>
+                </div>
+                <div class="next-prev">
+                    
+                </div>
+            </div>
 
-        </div>
-        <div class="answer">
-            <ul>
-                <li>A. dsdw</li>
-                <li>B. dsdw</li>
-                <li>C. dsdw</li>
-            </ul>
-        
+            <div class="col-md-3" id="wrapothernumber">
+                <div class="number_other">
+                    <span>NOMOR SOAL</span>
+                    <hr>
+                </div>
+            </div>
+
         </div>
     </div>
 
