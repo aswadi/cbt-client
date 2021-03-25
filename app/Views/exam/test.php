aaeb0e2c3920807ac4a7389cc78bb3691d7fdd02
aswadi <genta.blog@gmail.com>
2021-03-25 11:06:55 +0700

    change answer to radio

--- app/Views/exam/test.php
+++ app/Views/exam/test.php
@@ -15,16 +15,35 @@
                 <div class="clear"></div>
                 <hr>
                 <div class="question">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, voluptatibus. Placeat consectetur, obcaecati a laudantium voluptatem rem, accusantium, consequatur voluptates quod culpa rerum fugit esse doloribus maxime corrupti dicta enim!
-                    <hr>
+                    <div class="text">
+                        <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, voluptatibus. Placeat consectetur, obcaecati a laudantium voluptatem rem, accusantium, consequatur voluptates quod culpa rerum fugit esse doloribus maxime corrupti dicta enim!
+                        </p>
+                        <p>
+                        للترويح عن النفس
+                        </p>
+                        <img src="/assets/img/exam.jpg" alt="" srcset="" width="200px">
+                    </div>
+                    <div class="text-added">
+                        <p>اختر الأجوبة المناسبة للأسئلة التالية</p>   
+                    </div>
+                   
+                    
                 </div>
-                <div class="answer">
-                    <ul>
+                <hr>
+                <div class="answer pb-2">
+                    <!-- <ul>
                         <li>A. dsdw</li>
                         <li>B. dsdw</li>
                         <li>C. dsdw</li>
                         <li>D. dsdw</li>
-                    </ul>
+                    </ul> -->
+                    <p>Jawaban</p>
+                    <input type="radio" id="male" name="answer" value="male">
+                    <label for="male">Male</label><br>
+                    <input type="radio" id="female" name="answer" value="female">
+                    <label for="female">Female</label><br>
+                    <input type="radio" id="other" name="answer" value="other">
+                    <label for="other">Other</label>
                 </div>
                 <div class="prev-next">
                     <div class="prev">
